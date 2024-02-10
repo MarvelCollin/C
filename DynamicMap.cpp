29993fc562d57fd34f87cdbda6be606dad06df50
Marvel Collin <83052831+MarvelCollin@users.noreply.github.com>
2024-02-10 14:34:58 +0700

    Update DynamicMap.cpp
    feat: add calculation to count height adn width.
    fix: overprint height

--- DynamicMap.cpp
+++ DynamicMap.cpp
@@ -3,37 +3,62 @@
 #define ROWS 30
 #define COLS 130
 
+int calculateHeight(char layout[ROWS][COLS + 1]) {
+    int height = 0;
+    while (layout[height][0] != '\0') {
+        height++;
+    }
+    return height;
+}
+
+int calculateWidth(char layout[ROWS][COLS + 1]) {
+    int width = 0;
+    for (int i = 0; layout[0][i] != '\0'; i++) {
+        width++;
+    }
+    return width;
+}
+
 int main() {
     char layout[ROWS][COLS + 1] = {
     	"#####################################################",
-  		"#                                                   #",
-  		"#       #                                           #",
-  		"#       #                                           #",
-  		"#       #                                           #",
-  		"#       #                                           #",
-  		"#       #                                           #",
-  		"#                                         #         #",
-  		"#                                         #         #",
-  		"#                                         #         @",
-  		"#                       #                 #         #",
-  		"#                       #                 #         #",
-  		"#                      #                            #",
-  		"#                      #                            #",
-  		"#                      #                            #",
-  		"#                      #                            #",
-  		"#                                                   #",
-  		"#                                                   #",
-  		"#                                                   #",
-  		"#                                                   #",
-  		"#                                                   #",
-  		"#####################################################",
+		"#                                                   #",
+		"#       #                                           #",
+		"#       #                                           #",
+		"#       #                                           #",
+		"#       #                                           #",
+		"#       #                                           #",
+		"#                                         #         #",
+		"#                                         #         #",
+		"#                                         #         @",
+		"#                       #                 #         #",
+		"#                       #                 #         #",
+		"#                      #                            #",
+		"#                      #                            #",
+		"#                      #                            #",
+		"#                      #                            #",
+		"#                                                   #",
+		"#                                                   #",
+		"#                                                   #",
+		"#                                                   #",
+		"#                                                   #",
+		"#####################################################",
     };
 
 
-    //print the map
+    int height = calculateHeight(layout);
+    int width = calculateWidth(layout);
+
+    // print
     for (int i = 0; i < height; i++) {
         printf("%s\n", layout[i]);
     }
     
+    printf("Height of the map: %d\n", height);
+    printf("Width of the map: %d\n", width);
+	
+	while(1){
+		
+	}
     return 0;
 }
