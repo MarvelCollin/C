b57d70b00964858681bd8be4a0a336b2f325f814
Marvel Collin <83052831+MarvelCollin@users.noreply.github.com>
2024-07-03 15:18:15 +0700

    Update MaxHeap.cpp

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -1,105 +1,87 @@
 #include <stdio.h>
 
-int arr[100];
+int arr[1000];
 int size = 0;
 
-
-// dapatin parentnya
-int getParent(int pos){
-	return pos / 2;
+int getParentIndex(int position){
+    return position / 2;
 }
 
-// dapatin anakan kiri 
-int getLeftChild(int pos){
-	return pos * 2;
+int getLeftChild(int position){
+    return position * 2;
 }
 
-// dapatin anakan kanan
-int getRightChild(int pos){
-	return pos * 2 + 1;
+int getRightChildren(int position){
+    return position * 2 + 1;
 }
 
-// function utk tuker value
 void swap(int *a, int *b){
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void insert(int value){
-	size++; //buat buletannya (node) / siapin size
-	
-	arr[size] = value; // assign value
-	
-	int currIdx = size; // ganti penamaan
-	int parent = getParent(currIdx); // assign parent
-	
-	while(arr[currIdx] > arr[parent] && size > 1){ // apakah value current index > daripada parent
-		// untuk size > 1 digunakan utk kasus pertama kali insert
-		
-		// swap
-		swap(&arr[currIdx], &arr[parent]);
-		
-		// ganti current index untuk lanjut ngecheck
-		currIdx = parent;
-	}
+    size++;
+    arr[size] = value;
+    
+    int currentIndex = size;
+    int parent = getParentIndex(currentIndex);
+    
+    while(arr[currentIndex] > arr[parent] && currentIndex > 1){
+        swap(&arr[currentIndex], &arr[parent]);
+        currentIndex = parent;
+        parent = getParentIndex(currentIndex);
+    }
 }
 
-void heapify(int pos){
-	// kasih penamaan
-	int max = pos;
-	int leftChild = getLeftChild(pos);
-	int rightChild = getRightChild(pos);
-	
-	//apabila leftchild lebih besar daripada top
-	if(arr[leftChild] > arr[max] && leftChild <= size){
-		max = leftChild;
-	}
-	
-	if(arr[rightChild] > arr[max] && rightChild <= size){
-		max = rightChild;
-	}
-	
-	//check apakah maxnya ada keganti
-	if(pos != max){
-		//tuker
-		swap(&arr[pos], &arr[max]);
-		
-		
-		//lanjut check ke atas / recursive
-		heapify(max);
-	}
+void heapify(int position){
+    int max = position;
+    int leftChildrenIndex = getLeftChild(position);
+    int rightChildrenIndex = getRightChildren(position);
+    
+    if(arr[max] < arr[rightChildrenIndex]){
+        max = rightChildrenIndex;
+    }
+    if(arr[max] < arr[leftChildrenIndex]){
+        max = leftChildrenIndex;
+    }
+    
+    if(max != position){
+        swap(&arr[max], &arr[position]);
+        heapify(max);
+    }
 }
 
-void extract(){
-	// assign array atas ke array paling terakhir
-	arr[1] = arr[size];
-	
-	// hilangin sizenya
-	size--;
-	
-	
-	// heapify
-	heapify(1);
+void deleteHeap(){
+    if(size == 0){
+        printf("Heap empty");
+        return;
+    }
+    
+    arr[1] == arr[size];
+    size--;
+    
+    heapify(1);
 }
 
 void print(){
-	for(int i = 1; i <= size; i++){
-		printf("%d ", arr[i]);
-	}
-	printf("\nmax value : %d\n", arr[1]);
+    for(int i = 1; i <= size; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
 int main(){
-	insert(100);
-	insert(105);
-	insert(110);
-	
-	print();
-	extract();
-	print();
-	
-	insert(110);
-	print();
-	return 0;
+    insert(100);
+    insert(90);
+    insert(80);
+    insert(120);
+    
+    print();
+    
+    deleteHeap();
+    print();
+    
+    return 0;
 }
